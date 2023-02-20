2ef4c2481577062b3f8529fa92df62fb7a05d1d3
송연근 <daddy73e@mobmail.shinhan.com>
2023-02-20 14:22:58 +0900

    apple login

---
+++ GeuniWeb/UseCase/AppleLoginUseCase.swift
@@ -0,0 +1,27 @@
+//
+//  AppleLoginUseCase.swift
+//  GeuniWeb
+//
+//  Created by 60157085 on 2023/02/08.
+//
+
+import Foundation
+import AuthenticationServices
+
+public struct AppleLoginInput {
+    
+}
+public struct AppleLoginOutput {
+}
+
+public protocol AppleLoginUseCaseProtocol {
+    func execute(input: AppleLoginInput) -> AppleLoginOutput
+}
+
+public class AppleLoginUseCase: AppleLoginUseCaseProtocol {
+
+    public func execute(input: AppleLoginInput) -> AppleLoginOutput {
+        return AppleLoginOutput()
+    }
+}
+
